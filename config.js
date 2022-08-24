63b129625769bd3064701a66dec7bb12e53b798a
udithsasanga4 <111967336+udithsasanga4@users.noreply.github.com>
2022-08-24 14:32:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,20 +18,20 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '1(236)702-1498' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
-global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
+global.owner = ['1(236)702-1498',''] //මෙතන number එක.
+global.premium = ['1(236)702-1498','94778636881,0'] //මෙතන number එක.
+global.botname = '𝙐𝘿𝙄𝙏𝙃𝘼 𝘽𝙊𝙏' //මෙතන නම.
+global.ownername = '𝙐𝘿𝙄𝙏𝙃𝘼 𝙈𝘿🪀' //මෙතන නම.
+global.ownername = '𝙈𝙍.𝙐𝘿𝙄𝙏𝙃𝘼🪀' //මෙතන නම.
+global.group1 = 'https://chat.whatsapp.com/ElVTsg2HjFH2cX8qpjQnFd' //මෙතන grouplink එක.
+global.group2 = 'https://chat.whatsapp.com/ElVTsg2HjFH2cX8qpjQnFd' //මෙතන grouplink එක.
+global.packname = '𝙈𝙍.𝙐𝘿𝙄𝙏𝙃𝘼🪀' //මෙතන නම.
+global.cap = '*𝘽𝙔 𝙐𝘿𝙄𝙏𝙃𝘼 𝙈𝘿🪀*' //මෙතන නම.
+global.img = 'https://i.ibb.co/ypkhJZs/20220824-125351.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.ytchannel = 'https://youtube.com/channel/UChabFLlugrHRnsHbV_m-Jgg' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
 global.besiAwal = '15' //මෙතන මාරු කරන්න එපා.
@@ -43,7 +43,7 @@ global.dripstyping = false //මෙතන මාරු කරන්න එපා
 global.dripsreadgroup = false //මෙතන මාරු කරන්න එපා. 
 global.dripsreadall = false //මෙතන මාරු කරන්න එපා.
 global.dripsrecord = false //මෙතන මාරු කරන්න එපා.
-global.available = false //මෙතන මාරු කරන්න එපා.
+global.available = true //මෙතන මාරු කරන්න එපා.
 global.unavailable = true //මෙතන මාරු කරන්න එපා.
 global.limittot = 100, //මෙතන මාරු කරන්න එපා.
 global.f1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation' //මෙතන මාරු කරන්න එපා.
@@ -52,11 +52,11 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = '𝙐𝘿𝙄𝙏𝙃𝘼 𝙈𝘿🪀' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
-global.sp = '⭔' //මෙතන emoji එකක්.
+global.sp = '🪀' //මෙතන emoji එකක්.
 global.mess = {
     success: '*හරි*',
         ban: '*Sorry, looks like u got banned😔😔 type .owner To request unban okay*',
